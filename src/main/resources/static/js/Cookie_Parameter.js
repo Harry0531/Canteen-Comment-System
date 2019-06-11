3a9e24ead3c2372cffecfce0bd2d69b80f053c60
Zhangjiaming <916678091@qq.com>
2019-06-11 23:19:16 +0800

    2019-06-11 23:19

---
+++ src/main/resources/static/js/Cookie_Parameter.js
@@ -0,0 +1,26 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function GetRequest() {
+    var url = location.search; //获取url中"?"符后的字串
+    if (url.indexOf("?") != -1) {  //判断是否有参数
+        var str = url.substr(1); //从第一个字符开始 因为第0个是?号 获取所有除问号的所有符串
+        strs = str.split("=");  //用等号进行分隔 （因为知道只有一个参数 所以直接用等号进分隔 如果有多个参数 要用&号分隔 再用等号进行分隔）
+        //alert(strs[1]);     //直接弹出第一个参数 （如果有多个参数 还要进行循环的）
+        return strs[1];
+    }
+}
