010163a3de1fd3f3827abc2ece20357981ab71c2
Harry <1120171192@bit.edu.cn>
2019-06-12 13:34:44 +0800

    fix bug

--- src/main/java/com/uml_review/uml/UmlApplication.java
+++ src/main/java/com/uml_review/uml/UmlApplication.java
@@ -2,12 +2,18 @@ package com.uml_review.uml;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UmlApplication {
+public class UmlApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(UmlApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(UmlApplication.class);
+    }
 }
